fe457e30b6131aaa2b4473c4b4e67ebed85b6b1d
Jonatha32 <jonaperez038@gmail.com>
2024-12-10 20:07:21 +0000

    task 1, task 0, and binary_tree_print file

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ *
+ *
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *left_node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	left_node = malloc(sizeof(binary_tree_t));
+
+	if (left_node == NULL)
+		return (NULL);
+
+	left_node->n = value;
+	left_node->parent = parent;
+	left_node->left = parent->left;
+	left_node->right = NULL;
+
+	if (parent->left != NULL)
+	{
+		parent->left->parent = left_node;
+		parent->left = left_node;
+	}
+	return (left_node);
+}
