894d7efeac3d1d5f0b8c908e42480ab80bfa478e
aylin <pintosaylin7@gmail.com>
2024-12-11 23:32:29 +0000

    tasks 6, 7, 8

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: ptr to the root node of the tree to measure the height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (trees == NULL)
+		return (0);
+}
