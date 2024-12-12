eaeec5e725fa12c1d57c1e7d912a09c430f7cc84
Jonatha32 <jonaperez038@gmail.com>
2024-12-12 12:44:22 +0000

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,10 +3,21 @@
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: ptr to the root node of the tree to measure the height
+ * Return: size_t
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (trees == NULL)
+	size_t right, left;
+
+	if (tree == NULL)
+	{
 		return (0);
+	}
+
+	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return (left > right ? left : right);
+
 }
